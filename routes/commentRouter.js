22bd1360c276fbadb787ed328b59064b0259954a
mario salvatierra <msalv006@fiu.edu>
2019-08-05 17:09:51 -0400

    added route add new comment

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const Comment = require('../models/comments');
+const Dish = require('../models/dishes');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.status(200).json({hello: "world"} );
+  res.status(200).json({hello: "world all comment here"} );
   
 
 });
+
+router.post('/addcomment', (req, res, next) => {
+  const { rating , comment, author, dishid } = req.body;
+
+  Comment.create({rating: rating, comment: comment, author: author})
+  .then((commentx)=>{
+
+      Dish.findByIdAndUpdate(dishid, { $push:{comments: commentx._id}})
+      .then((response)=>{  res.json({response, commentx}) })
+      .catch(err=>{ res.json(err)  });
+  })
+  .catch(err=>{ res.json(err)  });
+
+  
+});
+
 
 module.exports = router;
